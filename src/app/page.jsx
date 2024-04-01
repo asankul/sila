5975dc782442d81db5110fbb1aacf624a29e1e31
asankul <asankul1993@gmail.com>
2024-04-01 23:49:58 +0300

    all

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,8 @@ import { useEffect } from 'react';
 
 const RedirectPage = () => {
   if (typeof window !== 'undefined') {
-    // Code that relies on window object
-    // For example:
     window.location.href = '/login';
-  }
+  }  
 };
 
 export default RedirectPage;
