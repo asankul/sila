c99f484b94eb6696a15b1f44d68f2718b2ee9a1a
asankul <asankul1993@gmail.com>
2024-04-01 23:34:28 +0300

    all

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,9 @@
 "use client"
 import { useEffect } from 'react';
 
+
 const RedirectPage = () => {
-  useEffect(() => {
-    const redirectTimer = setTimeout(() => {
-      // Redirect to a different URL after 3 seconds
-      window.location.href = '/login'; // Replace '/new-url' with the URL you want to redirect to
-    }, 0); // 3000 milliseconds = 3 seconds
-
-    return () => clearTimeout(redirectTimer); // Clear the timer on unmount
-  }, []);
-
-  return (
-    <div>
-      <img src="/logo.png" alt="Logo" class="logo"/>
-    </div>
-  );
+  window.location.href = '/login';
 };
 
 export default RedirectPage;
